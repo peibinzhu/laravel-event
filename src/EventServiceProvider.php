2139b3fecebef8a7e02b40731a0e4b0faae5cdc4
peibinzhu <760425373@qq.com>
2023-03-03 20:07:48 +0800

    Initial release.

--- src/EventServiceProvider.php
+++ src/EventServiceProvider.php
@@ -4,15 +4,19 @@ declare(strict_types=1);
 
 namespace PeibinLaravel\Event;
 
+use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Support\ServiceProvider;
+use PeibinLaravel\SwooleEvent\Events\BootApplication;
 
 class EventServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->app->get(ListenerProviderFactory::class)();
-
         $this->app->singleton(ListenerProvider::class);
         $this->app->get(ListenerProvider::class);
+
+        $this->app->get(Dispatcher::class)->listen(BootApplication::class, function () {
+            $this->app->get(ListenerProviderFactory::class)();
+        });
     }
 }
